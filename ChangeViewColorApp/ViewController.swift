9b37cc7f70f74b4df114cb542a80dec9a79e9f51
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-12 00:59:34 +0300

    Change constraints

--- ChangeViewColorApp/ViewController.swift
+++ ChangeViewColorApp/ViewController.swift
@@ -12,54 +12,42 @@ class ViewController: UIViewController {
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
+    
     @IBOutlet var redCount: UILabel!
     @IBOutlet var greenCount: UILabel!
     @IBOutlet var blueCount: UILabel!
+    
     @IBOutlet var mainView: UIView!
  
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mainView.layer.cornerRadius = 20
+        mainView.backgroundColor = changeViewColor()
        
         redSlider.value = 0.3
         greenSlider.value = 0.5
         blueSlider.value = 0.7
-        
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 1
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 1
-        
     }
-   
-    @IBAction func redSliderAction() {
+    
+    @IBAction func sliderAction() {
         redCount.text = String(format: "%.1f", redSlider.value)
+        greenCount.text = String(format: "%.1f", greenSlider.value)
+        blueCount.text = String(format: "%.1f", blueSlider.value)
+        
+        mainView.backgroundColor = changeViewColor()
+    }
+    
+    func changeViewColor() -> UIColor {
         let redSliderValue = CGFloat(redSlider.value)
         let greenSliderValue = CGFloat(greenSlider.value)
         let blueSliderValue = CGFloat(blueSlider.value)
-        mainView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: redSliderValue)
-    }
     
-    @IBAction func greenSliderAction() {
-        greenCount.text = String(format: "%.1f",greenSlider.value)
-        let redSliderValue = CGFloat(redSlider.value)
-        let greenSliderValue = CGFloat(greenSlider.value)
-        let blueSliderValue = CGFloat(blueSlider.value)
-        mainView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: greenSliderValue)
+        return UIColor(red: redSliderValue,
+                       green: greenSliderValue,
+                       blue: blueSliderValue,
+                       alpha: 1)
     }
-    
-    @IBAction func blueSliderAction() {
-        blueCount.text = String(format: "%.1f",blueSlider.value)
-        let redSliderValue = CGFloat(redSlider.value)
-        let greenSliderValue = CGFloat(greenSlider.value)
-        let blueSliderValue = CGFloat(blueSlider.value)
-        mainView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: blueSliderValue)
-    }
-    
 }
 
